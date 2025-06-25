104a3fd5f3f8fc2670c10380db415b70f5b37c19
apsakti <147362634+apsaktiproject@users.noreply.github.com>
2025-06-25 15:05:23 +0700

    initial

---
+++ src/main/java/org/testapi/TestApi0Application.java
@@ -0,0 +1,13 @@
+package org.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApi0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApi0Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/testapi/TestApi0ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApi0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
