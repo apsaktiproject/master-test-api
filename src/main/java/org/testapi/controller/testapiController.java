bb2a0e3aef13019d73cbb547816187df181e2721
apsakti <147362634+apsaktiproject@users.noreply.github.com>
2025-06-27 22:11:39 +0700

    move controller to controller package

--- src/main/java/org/testapi/controller/testapiController.java
+++ src/main/java/org/testapi/controller/testapiController.java
@@ -1,14 +1,14 @@
-package org.testapi;
+package org.testapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testapiController {
 	
-	@RequestMapping("/greeting")
+	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
 	public String welcome () {
 		return "welcome to hell";
 	}
